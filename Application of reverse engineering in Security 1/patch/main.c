c83acaeb028a72171224cd47dd01329541d5478e
n0s1kh1ya <gauravkumaryadav618@gmail.com>
2020-11-15 16:20:01 +0530

    Executable and project for part1

---
+++ Application of reverse engineering in Security 1/patch/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void onSuccessful()
+{
+    printf("You can now access me Thank you for purchasing my license!");
+    exit(0);
+}
+void onFail()
+{
+    printf("Wrong product key!");
+    return;
+}
+
+
+int main()
+{
+    int pdKey;
+    printf("Welcome to patch!");
+
+
+    while(1)
+    {
+        printf("\n");
+        printf("You need to enter a right product key to access me!");
+        printf("\n");
+        scanf("%d",&pdKey);
+
+        if(pdKey == 534728)
+        {
+            onSuccessful();
+        }
+        else
+        {
+            onFail();
+        }
+    }
+
+    return 0;
+}
